100e9901494cfb5fddd9202162dfce45d62566c9
Ali Ahmad Faiz Ahmad <afaiz-ahmad@myseneca.ca>
2025-01-08 19:09:23 -0500

    Added lab1b

---
+++ lab1b.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+'''
+This program will ask a question
+Author: Afaiz-ahmad
+'''
+
+#name of the individual
+name = 'Isaac'
+
+# message to the user
+print ('How old are you ' + name + '?' )
