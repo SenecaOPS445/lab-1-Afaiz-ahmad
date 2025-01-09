4bab701f6f1ff4ee8999abc9786bc212e30c1893
Ali Ahmad Faiz Ahmad <afaiz-ahmad@myseneca.ca>
2025-01-08 20:37:15 -0500

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+'''
+This program will perform mathematical operations
+Author: Afaiz-ahmad
+'''
+
+x = 10
+y = 2
+z = 5
+
+result = x + y * z
+
+print (str(x) + ' + ' + str(y) + ' * ' + str(z) + ' = ' + str(result))
